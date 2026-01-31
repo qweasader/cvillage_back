c5bc14dbfea0af035d2ef6fb96dd03c662b6a331
qweasader <52488815+qweasader@users.noreply.github.com>
2026-01-31 22:13:11 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ import 'dotenv/config';
 
 // ==================== КОНФИГУРАЦИЯ ====================
 const ADMIN_USER_IDS = [
-  131918408
+  131918408,
 ];
 
 const TELEGRAM_BOT_TOKEN = process.env.TELEGRAM_BOT_TOKEN;
